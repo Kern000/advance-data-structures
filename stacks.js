4770303d0cafe2589f5969a15399e760fe41c4d4
unknown <holupkern@gmail.com>
2023-12-04 18:36:17 +0800

    updated Stack method of add multiple - replaced concat which created new array

--- stacks.js
+++ stacks.js
@@ -38,12 +38,27 @@ class Stack {
 
     addMultipleToStack(array){
         if (Array.isArray(array)){
-            let combinedStack = this.items.concat(array);
-            return combinedStack;
+            for (let value of array){
+                this.items.push(value);
+            }
+            return this.items;
         } else {
             throw new Error("Argument not an array");
         }
     }
 }
 
-export default Stack;
+// testing start //
+let stack1 = new Stack;
+stack1.addOneToStack(5);
+stack1.addMultipleToStack([1,2,3,4])
+console.log(stack1.isEmpty());
+console.log("Top of stack => ", stack1.viewTopOfStack());
+console.log("Size of stack => ", stack1.sizeOfStack());
+console.log(stack1);
+
+
+// testing end //
+
+module.exports = Stack;
+
