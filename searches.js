d2c15511b220e8193f14694ea953a9d1469fb9f1
metalizechicken <holupkern@gmail.com>
2023-12-06 02:40:54 +0800

    planning for search - hashing is alr a fast form of search, array binary search will be coded here

---
+++ searches.js
@@ -0,0 +1,2 @@
+// hashing gives O(1) time complexity average case lookups, using a hash function to map to indices of an array; This is one of the fastest search.
+// will only code array binary search here;
