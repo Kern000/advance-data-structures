6d06c74039cc206d84cef61d89a36f140a1f802a
metalizechicken <holupkern@gmail.com>
2023-12-06 03:12:24 +0800

    added time complexity for diff Graph representation

---
+++ dijkstraAlgo.js
@@ -0,0 +1,2 @@
+// shortest path in weighted graph (I find it easiest in adjacency matrix)
+// longest path - apply * -1 to edges, and convert back later with * -1
--- graphs.js
+++ graphs.js
@@ -1,1 +1,6 @@
 // Every tree will always be a graph but not all graphs will be trees. Linked List, Trees, and Heaps all are special cases of graphs.
+
+// Action	        Adjacency Matrix	Adjacency List
+// Adding Edge	        O(1)	        O(1)
+// Removing an edge	    O(1)	        O(N)
+// Initializing	        O(N*N)	        O(N)
