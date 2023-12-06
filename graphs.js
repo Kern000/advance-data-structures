70313668e2d06a8982ae7611c1b69cef4e59e283
unknown <holupkern@gmail.com>
2023-12-06 18:35:58 +0800

    graph notes theory updated

--- graphs.js
+++ graphs.js
@@ -4,3 +4,34 @@
 // Adding Edge	        O(1)	        O(1)
 // Removing an edge	    O(1)	        O(N)
 // Initializing	        O(N*N)	        O(N)
+
+// have a node or vertex
+// edge - pair (u,v) not same as (v,u); if unweighted set to 1.
+// node v adj to u if edge(u,v) exist
+
+//indegree - no. arrivals to a node
+//outdegree - no. departures from a node
+
+// path P(u,v) = (v0, v1, v2... vn);
+
+//isolated node can BFS
+// directed graph => Tree and directed Acyclic graph
+// undirected => one of them being connected graph w no isolated node
+// other graph (Bipartite - edge from each vi to wj)
+
+//parallel edge - 2 vertice connected with more than 1 edge
+
+// walk = traverse where vertex can be repeated
+// trail = traverse where vertex cannot be repeated
+// circuit = traverse where edge cannot be repeated
+// path = both vertex and edge not repeated
+// cycle = both not repeated, but end on start node
+
+
+
+
+
+
+
+
+
